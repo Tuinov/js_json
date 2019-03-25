102f786ee2fb6299f9d4731de0d5ebe2a6cc3f4e
EvgeniyTuinov <47924160+EvgeniyTuinov@users.noreply.github.com>
2019-03-25 22:58:18 +0300

    запросил json файл с сервера и распарсил. Обернул функцию в промис.

---
+++ ajax.js
@@ -0,0 +1,17 @@
+// http(s) - hyper text transport protocol secure
+// client (browser) ->(request) server
+// server ->(response) client
+
+// headers
+// body
+
+// http-method(type):
+/*
+- GET 
+- POST 
+- PUT(full update)/PATCH(partial update)
+- DELETE
+- OPTIONS
+*/
+
+// AJAX - Asynchronus Javascript and XML
